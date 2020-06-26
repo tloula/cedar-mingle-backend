b4e66fbed4b64f88c9171020c0007882716f476c
Trevor Loula <trevorloula@outlook.com>
2020-06-26 11:29:19 -0500

    Updated DB Schema

--- dbschema.js
+++ dbschema.js
@@ -1,11 +1,23 @@
 let db = {
-    screams: [
-        {
-            user: 'user',
-            body: 'this is the scream body',
-            timestamp: '2020-06-25T18:44:59.546Z',
-            likeCount: 5,
-            commentCount: 2
-        }
-    ]
-}
+  users: [
+    {
+      userId: "klsafjaslkfjlsakfj",
+      email: "user@cedarville.edu",
+      username: "user",
+      createdAt: "2019-03-15T10:59:52.798Z",
+      imageUrl: "image/adlfkjasdlasdfd/dasfdsfds",
+      bio: "Hello, my name is user, nice to meet you",
+      website: "https://user.com",
+      location: "Chicago, IL",
+    },
+  ],
+  screams: [
+    {
+      user: "user",
+      body: "this is the scream body",
+      createdAt: "2020-06-25T18:44:59.546Z",
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
