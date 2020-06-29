fa9e7373be2531158b76efb32eb738ff2320775e
Trevor Loula <trevorloula@outlook.com>
2020-06-28 21:54:31 -0500

    Updated Schema

--- dbschema.js
+++ dbschema.js
@@ -1,12 +1,15 @@
 let db = {
+  // Each document has a max size of 1 MiB (1,048,576 bytes)
+  // 1 Byte is 1 8-Bit ASCII Character
+  // Approximately 26064 39-Character Lines Fit in 1024 KiB
   users: [
     {
       userId: "5mMMdx1E1QbV2Lb7CjcU6qES1Sj1",
       email: "john@cedarville.edu",
       name: "John",
+      gender: "male",
       birthday: "01/01/2000",
       gradYear: "2021",
-      gender: "male",
       major: "Computer Engineering",
       hometown: "Chicago, IL",
       about: "Hello, my name is user, nice to meet you",
@@ -21,23 +24,23 @@ let db = {
       boost: false,
       swipes: 15, // Reset every night
       discoverableParty: "liked",
-      ageRange: "18-21",
+      discoverableAgeRange: "18-21",
       createdAt: "2019-03-15T10:59:52.798Z",
       likes: {
-        userId: "klsafjaslkfjlsakfj",
-        userId: "klsafjaslkfjlsakfj",
+        userId: "5mMMdx1E1QbV2Lb7CjcU6qES1Sj1",
+        userId: "5mMMdx1E1QbV2Lb7CjcU6qES1Sj1",
       },
       dislikes: {
-        userId: "klsafjaslkfjlsakfj",
-        userId: "klsafjaslkfjlsakfj",
+        userId: "5mMMdx1E1QbV2Lb7CjcU6qES1Sj1",
+        userId: "5mMMdx1E1QbV2Lb7CjcU6qES1Sj1",
       },
       receivedLikes: {
-        userId: "klsafjaslkfjlsakfj",
-        userId: "klsafjaslkfjlsakfj",
+        userId: "5mMMdx1E1QbV2Lb7CjcU6qES1Sj1",
+        userId: "5mMMdx1E1QbV2Lb7CjcU6qES1Sj1",
       },
       receivedDislikes: {
-        userId: "klsafjaslkfjlsakfj",
-        userId: "klsafjaslkfjlsakfj",
+        userId: "5mMMdx1E1QbV2Lb7CjcU6qES1Sj1",
+        userId: "5mMMdx1E1QbV2Lb7CjcU6qES1Sj1",
       },
       matches: [
         {
