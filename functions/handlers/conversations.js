e1630e6578ec1c6b49ca254853a39fa5350948b8
Trevor Loula <trevorloula@outlook.com>
2020-07-06 19:39:14 -0500

    Created getConversation Route

--- functions/handlers/conversations.js
+++ functions/handlers/conversations.js
@@ -8,7 +8,7 @@ exports.getAllConversations = (req, res) => {
     .where("uids", "array-contains", req.user.uid)
     .get()
     .then((docs) => {
-      conversations = [];
+      let conversations = [];
       docs.forEach((doc) => {
         let names = doc.data().names;
         let uids = doc.data().uids;
@@ -38,7 +38,44 @@ exports.getAllConversations = (req, res) => {
 
 // Get Specific Conversation Route
 exports.getConversation = (req, res) => {
-  return res.status(200).json({ message: "Success" });
+  const conversation = db.doc(`/conversations/${req.params.cid}`);
+  conversation
+    .get()
+    .then((doc) => {
+      let names = doc.data().names;
+      let uids = doc.data().uids;
+      let name;
+      let uid;
+
+      if (names[0] === req.user.name) name = names[1];
+      else name = names[0];
+
+      if (uids[0] === req.user.uid) uid = uids[0];
+      else uid = uids[0];
+
+      conversation
+        .collection("messages")
+        .get()
+        .then((messages) => {
+          let data = [];
+          messages.forEach((message) => {
+            data.push(message.data());
+          });
+          return res.status(200).json({ name, uid, messages: data });
+        })
+        .catch((err) => {
+          console.error(err);
+          return res
+            .status(500)
+            .json({ error: "Internal error retrieving messages" });
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res
+        .status(500)
+        .json({ error: "Internal error retrieving conversation" });
+    });
 };
 
 // Message User Route
