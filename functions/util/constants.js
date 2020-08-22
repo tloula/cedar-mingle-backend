dfa4514e66051020f95557d77c5b4d54945a4205
Trevor Loula <trevorloula@outlook.com>
2020-08-22 15:09:51 -0400

    Updated support emails

--- functions/util/constants.js
+++ functions/util/constants.js
@@ -2,8 +2,8 @@ module.exports = {
   MAX_SWIPES: 25,
   MIN_USER_AGE: 16,
   EMAIL_DOMAIN: "@cedarville.edu",
-  NO_REPLY_EMAIL: "Cedar Mingle <contact@trevorloula.com>",
-  SUPPORT_EMAIL: "Cedar Mingle Support <contact@trevorloula.com>",
+  NO_REPLY_EMAIL: "Cedar Mingle <no-reply@cedarmingle.com>",
+  SUPPORT_EMAIL: "Cedar Mingle Support <support@cedarmingle.com>",
   APP_NAME: "Cedar Mingle",
   APP_URL: "https://cedarmingle.com",
   REQUIRE_VERIFIED_EMAIL: false, // Set to false for testing
